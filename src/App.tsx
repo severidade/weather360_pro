1d6b7e1859b067fc705f8671393e4ab780ef95db
severo <oliveira.severo@gmail.com>
2024-08-27 00:43:12 -0300

    feat: :rocket: Iniciando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+// eslint-disable-next-line import/no-unresolved
+
+import './App.css';
+
+function App() {
+  return (
+    <p>ola</p>
+  );
+}
+
+export default App;
